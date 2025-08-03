b9144f80efc8ae7ef3e09aaa7d052eb551bbdce0
willy1830 <143012193+willy1830@users.noreply.github.com>
2025-08-03 10:11:33 -0400

    main.js

--- proyecto-hidraulica/static/js/main.js
+++ proyecto-hidraulica/static/js/main.js
@@ -1,1 +1,7 @@
+setTimeout(() => {
+  const intro = document.getElementById('intro');
+  const mainSection = document.querySelector('section');
 
+  intro.style.display = 'none';        // Oculta overlay
+  mainSection.classList.add('visible'); // Inicia fade-in
+}, 4000);
